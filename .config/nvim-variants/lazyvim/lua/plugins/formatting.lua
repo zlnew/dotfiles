2800480696f6cf03239817107b1a505d10e8342a
Maulana Aprizqy Sumaryanto <maulana4pz@gmail.com>
2025-05-20 09:05:54 +0700

    fix: add PHP Pint formatter to conform.nvim config and migrate PHP test setup to neotest-pest
    - Added `pint` as PHP formatter in conform.nvim for better PHP code
    formatting
    - Renamed `php.lua` plugin config to `testing.lua` and replaced PHP
    formatter setup with neotest-pest integration
    - Streamlined test tooling to use neotest ecosystem for Pest support,
    dropping redundant PHP formatter config

--- .config/nvim-variants/lazyvim/lua/plugins/formatting.lua
+++ .config/nvim-variants/lazyvim/lua/plugins/formatting.lua
@@ -3,6 +3,7 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
+      php = { "pint" },
       javascript = { "prettierd" },
       typescript = { "prettierd" },
       vue = { "prettierd" },
--- .config/nvim-variants/lazyvim/lua/plugins/testing.lua
+++ .config/nvim-variants/lazyvim/lua/plugins/testing.lua
@@ -1,14 +1,5 @@
 return {
   {
-    "stevearc/conform.nvim",
-    optional = true,
-    opts = {
-      formatters_by_ft = {
-        php = { "pint" },
-      },
-    },
-  },
-  {
     "nvim-neotest/neotest",
     dependencies = { "V13Axel/neotest-pest" },
     opts = {
