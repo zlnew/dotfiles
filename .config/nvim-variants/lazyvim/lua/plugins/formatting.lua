49e15519258b56c48615429eadfc9f10b4a20f59
Maulana Aprizqy Sumaryanto <maulana4pz@gmail.com>
2025-07-08 12:18:17 +0700

    Add `eslint_d` as secondary formatter for TS and Vue
    - Updated `formatters_by_ft` to include `eslint_d` alongside `prettierd`
    for `typescript` and `vue` filetypes.
    - Enhances formatting by combining stylistic fixes (`prettierd`) with
    linting rule enforcement (`eslint_d`).

--- .config/nvim-variants/lazyvim/lua/plugins/formatting.lua
+++ .config/nvim-variants/lazyvim/lua/plugins/formatting.lua
@@ -5,8 +5,8 @@ return {
     formatters_by_ft = {
       php = { "pint" },
       javascript = { "prettierd" },
-      typescript = { "prettierd" },
-      vue = { "prettierd" },
+      typescript = { "prettierd", "eslint_d" },
+      vue = { "prettierd", "eslint_d" },
       json = { "prettierd" },
       css = { "prettierd" },
       scss = { "prettierd" },
