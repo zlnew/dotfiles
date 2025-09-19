1974d64d001d445b6d110e9ac66a820709f32e5f
Maulana Aprizqy Sumaryanto <maulana4pz@gmail.com>
2025-09-19 09:44:13 +0700

    chore(dotfiles): update configs (2025-09-19 09:44)

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,24 +1,22 @@
 return {
   {
-    "ellisonleao/gruvbox.nvim",
+    "sainnhe/gruvbox-material",
     priority = 1000,
-    config = true,
-    opts = {
-      italic = {
-        strings = false,
-        emphasis = false,
-        comments = false,
-        operators = false,
-        folds = false,
-      },
-      constrast = "soft",
-      transparent_mode = true,
-    },
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
-    },
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.gruvbox_material_enable_italic = true
+      vim.g.gruvbox_material_transparent_background = "2"
+      vim.cmd.colorscheme("gruvbox-material")
+
+      -- 🔑 Force floating windows & terminals to be transparent
+      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+      vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+      vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+      vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+      vim.api.nvim_set_hl(0, "TermNormal", { bg = "none" })
+      vim.api.nvim_set_hl(0, "TermNormalNC", { bg = "none" })
+    end,
   },
 }
