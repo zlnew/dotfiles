0e09c48ff944ecbb298913021585fafbe2915033
Maulana Aprizqy Sumaryanto <maulana4pz@gmail.com>
2025-09-20 13:43:37 +0700

    chore(dotfiles): update configs (2025-09-20 13:43)

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -2,3 +2,4 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 vim.o.shellcmdflag = "-ic"
+vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
