206333644fce8f98cc970b4d81105510a4696623
Maulana Aprizqy Sumaryanto <maulana4pz@gmail.com>
2025-09-20 20:36:22 +0700

    chore(dotfiles): update configs (2025-09-20 20:36)

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,6 +1,4 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
-  { "snacks.nvim", opts = { indent = { enabled = false } } },
-  { "folke/noice.nvim", enabled = false },
 }
