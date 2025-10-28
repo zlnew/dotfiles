a0820251e4587ddb751c844339859f3a6999d4de
Maulana Aprizqy Sumaryanto <maulana4pz@gmail.com>
2025-10-28 20:58:23 +0700

    chore(dotfiles): update configs (2025-10-28 20:58)

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "vue", "javascript", "typescript", "css", "html" })
+      vim.list_extend(opts.ensure_installed, { "vue", "javascript", "typescript", "css", "html", "php" })
 
       -- enable context_commentstring
       opts.context_commentstring = {
