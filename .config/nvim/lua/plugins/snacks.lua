8f37b89760b08b5f97217323c15b0775cf8161f8
Maulana Aprizqy Sumaryanto <maulana4pz@gmail.com>
2025-10-29 08:32:21 +0700

    chore(dotfiles): update configs (2025-10-29 08:32)

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -2,10 +2,20 @@ return {
   "snacks.nvim",
   opts = {
     picker = {
+      notifier = {
+        enabled = true,
+        level = vim.log.levels.ERROR,
+        style = "minimal"
+      },
+      scroll = { enabled = false },
+      animate = { enabled = false },
       sources = {
         files = {
           hidden = true,
           ignored = false,
+          layout = {
+            fullscreen = true
+          }
         },
         grep = {
           hidden = true,
@@ -14,6 +24,12 @@ return {
         explorer = {
           hidden = true,
           ignored = true,
+          auto_close = true,
+          layout = {
+            preview = "preview",
+            preset = "ivy",
+            hidden = { "input" }
+          }
         },
       },
       formatters = {
