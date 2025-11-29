d986da7d45f502a0bb9e275d64cc8844c463d86e
Maulana Aprizqy Sumaryanto <maulana4pz@gmail.com>
2025-11-29 21:49:58 +0700

    chore(dotfiles): update configs (2025-11-29 21:50)

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -57,12 +57,31 @@ return {
         },
       }
 
+      local gopls_ls_config = {
+        settings = {
+          gopls = {
+            gofumpt = true,
+            staticcheck = true,
+            analyses = {
+              unusedparams = true,
+            },
+          },
+        },
+        filetypes = {
+          "go",
+          "gomod",
+          "gowork",
+          "gotmpl",
+        },
+      }
+
       opts.servers = opts.servers or {}
       opts.servers.vtsls = vim.tbl_deep_extend("force", opts.servers.vtsls or {}, vts_ls_config)
       opts.servers.volar = vim.tbl_deep_extend("force", opts.servers.volar or {}, vue_ls_config)
       opts.servers.tailwindcss = vim.tbl_deep_extend("force", opts.servers.tailwindcss or {}, tailwindcss_ls_config)
       opts.servers.intelephense = vim.tbl_deep_extend("force", opts.servers.intelephense or {}, intelephense_ls_config)
       opts.servers.phpactor = vim.tbl_deep_extend("force", opts.servers.phpactor or {}, phpactor_ls_config)
+      opts.servers.gopls = vim.tbl_deep_extend("force", opts.servers.gopls or {}, gopls_ls_config)
     end,
   },
 }
