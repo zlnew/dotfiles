d5a868b7a20d81a3785434c4ee216eb8596c756c
Maulana Aprizqy Sumaryanto <maulana4pz@gmail.com>
2025-11-30 08:19:46 +0700

    chore(dotfiles): update configs (2025-11-30 08:19)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,10 +15,16 @@ if status is-interactive
 end
 
 set --export BUN_INSTALL "$HOME/.bun"
-set --export PHPENV_ROOT "$HOME/.phpenv"
+set --export PHPENV_ROOT /srv/phpenv
+set --export GO_ROOT /usr/local/go
+
 set --export PATH $BUN_INSTALL/bin $PATH
 set --export PATH "$PHPENV_ROOT/bin" $PATH
 set --export PATH "$HOME/.config/composer/vendor/bin" $PATH
+set --export PATH "$GO_ROOT/bin" $PATH
+
+set -Ux GOPATH (go env GOPATH)
+set -gx PATH $GOPATH/bin $PATH
 
 if type -q zoxide
     zoxide init fish | source
